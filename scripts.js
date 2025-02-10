418b6d9a5726b49ad088a00d389a38a57eb1a253
codeur2point0 <codeurdeuxpoint0@gmail.com>
2025-02-10 01:06:04 +0000

    ajout de preloader

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ function showMessage(section) {
         'passion': "Ma passion",
         'contact': "Contact"
     };
-   
+
     const messages = {
         'about': `
         <div style="display: flex; align-items: center; gap: 20px;">
@@ -33,6 +33,9 @@ function showMessage(section) {
                     <li>Création d'interfaces responsives</li>
                     <li>Optimisation des performances</li>
                 </ul>
+                 Je m'adapte facilement au travail en équipe. En tant que nouveau diplômé en quête d'emploi, je suis désireux de renforcer mes compétences en développement web et mobile.
+                 J'ai participé à divers projets académiques et personnels, notamment une plateforme web qui met en relation des clients et des professionnels du métier artisanal. Ces expériences ont enrichi mes compétences en création d’interfaces responsives et optimisation des performances.
+                 Je recherche une première expérience professionnelle qui me permettra d'apprendre et de contribuer à des projets innovants tout en développant mes compétences en développement web et mobile.
             </div>
             <div style="flex-shrink: 0;">
                 <img src="user.png" alt="Ma photo" style="max-width: 150px; border-radius: 10px;">
@@ -48,6 +51,10 @@ function showMessage(section) {
             en m'assurant que tout fonctionne harmonieusement en coulisses.
             Cela me permet d'avoir une vision globale des projets que je réalise
             et de créer des solutions complètes et efficaces.</strong>
+            En outre, je suis constamment à la recherche de nouvelles technologies et de tendances du secteur pour rester à jour et améliorer mes compétences. Je participe régulièrement à des forums de développeurs et à des ateliers, ce qui me permet d'échanger des idées avec d'autres passionnés et d'apprendre de nouvelles techniques.
+            L'un des aspects que j'apprécie le plus dans le développement web est la possibilité de résoudre des problèmes complexes et de relever des défis techniques. Chaque projet est unique, et la diversité des problématiques à traiter stimule ma créativité.
+            Je m'efforce également d'optimiser le code et d'améliorer les performances des applications, car je suis convaincu que même les détails les plus petits peuvent avoir un impact significatif sur l'expérience utilisateur. Je valorise la collaboration avec des designers et d'autres développeurs pour créer des produits qui non seulement fonctionnent bien, mais qui sont également visuellement attrayants et accessibles.
+            Dans le futur, j'espère continuer à explorer des domaines comme le développement front-end et back-end, l'intégration d'intelligences artificielles dans les applications, et l'optimisation pour les appareils mobiles. Mon objectif est de contribuer à des projets innovants qui apportent une valeur ajoutée aux utilisateurs tout en m'épanouissant personnellement et professionnellement dans cette discipline.
         </p>
     `,
         'contact': `
